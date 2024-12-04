eb107fb3056a8fb2bac57f0d857dfffa23eae94d
mrm6963 <mrm6963@psu.edu>
2024-12-04 09:59:38 -0500

    dream you activity complete... jsut needs on click functionality

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/DreamHouseActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/DreamHouseActivity.java
@@ -9,6 +9,6 @@ public class DreamHouseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dreamhouse);
+        setContentView(R.layout.activity_dreamcharacter);
     }
 }
--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/DreamYouActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/DreamYouActivity.java
@@ -1,9 +1,12 @@
 package edu.psu.ist.hcdd340.finalproject;
 
 
+import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.imageview.ShapeableImageView;
 
 final class CharacterProfile{
     private final String characterName;
@@ -24,7 +27,16 @@ final class CharacterProfile{
     }
 
     public class DreamYouActivity extends AppCompatActivity implements View.OnClickListener{
+
         public static final String TAG = "DREAM_LIFE_ACTIVITY";
+
+        private final int[] ACTION_INCON_IDS =
+                {
+                  R.id.prev_button,
+                  R.id.next_button,
+                  R.id.next_button
+
+                };
 
         private final CharacterProfile[] CHARACTER_PROFILES = {
                 new CharacterProfile("Hercules", R.drawable.hercules),
@@ -32,15 +44,57 @@ final class CharacterProfile{
                 new CharacterProfile("Cleopatra", R.drawable.cleopatra),
                 new CharacterProfile("Fancy Lady", R.drawable.fancylady),
                 new CharacterProfile("Lady", R.drawable.lady),
-                new CharacterProfile("Some Guy", R.drawable.someguy)};
+                new CharacterProfile("Some Guy", R.drawable.someguy)
+        };
+
+        private int index = 0;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_dreamcharacter);
+
+            //Set event handler for icons
+            ShapeableImageView prev = findViewById(R.id.prev_button);
+            prev.setOnClickListener(this);
+
+            ShapeableImageView save = findViewById(R.id.save_button);
+            save.setOnClickListener(this);
+
+            ShapeableImageView next = findViewById(R.id.next_button);
+            next.setOnClickListener(this);
+        }
 
         @Override
         public void onClick(View view) {
+        int eventSourceId = view.getId();
+
+        CharacterProfile characterProfile = getCurrentProfile();
+
 
         }
+
+        private CharacterProfile moveToNextProfile()
+        {
+            index= (index + 1) % CHARACTER_PROFILES.length;
+            return CHARACTER_PROFILES[index];
+        }
+
+        private CharacterProfile moveToPreviousProfile()
+        {
+            index = index - 1;
+            if (index <0)
+                index = CHARACTER_PROFILES.length - 1;
+            return CHARACTER_PROFILES[index];
+        }
+
+        private CharacterProfile getCurrentProfile ()
+        {
+            return CHARACTER_PROFILES[index];
+        }
+
+
     }
-
-
 }
 
 
