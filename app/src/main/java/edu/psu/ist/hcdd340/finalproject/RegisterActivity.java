4229879bdd2dd64954f2fc6f9241a31d76b94a0d
mrm6963 <mrm6963@psu.edu>
2024-12-12 17:18:01 -0500

    register dialog box

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/RegisterActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/RegisterActivity.java
@@ -1,19 +1,16 @@
 package edu.psu.ist.hcdd340.finalproject;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -26,7 +23,6 @@ public class RegisterActivity extends AppCompatActivity  implements View.OnClick
     public static final String PASSWORD_KEY = "PASSWORD";
     public static final String FIRST_NAME_KEY = "FIRST_NAME";
     public static final String LAST_NAME_KEY = "LAST_NAME";
-    public static final String COLLEGE_KEY = "COLLEGE";
 
     private SharedPreferences sharedPreferences;
 
@@ -47,16 +43,32 @@ public class RegisterActivity extends AppCompatActivity  implements View.OnClick
 
     @Override
     public void onClick(View view) {
+
+        String email = getInputFromEditText(R.id.editTextEmail);
+        String password = getInputFromEditText(R.id.editTextPassword);
+        String firstName = getInputFromEditText(R.id.editFirstName);
+        String lastName = getInputFromEditText(R.id.editLastName);
+
         int id = view.getId();
         if (id == R.id.button_cancel_registration) {
-            finish();
-        } else if (id == R.id.button_register) {
+            finishAffinity();
+        } else if (id == R.id.button_register && !email.isEmpty() && !password.isEmpty() &&
+        !firstName.isEmpty() && !lastName.isEmpty()) {
             registerUser();
             Button button = findViewById(R.id.button_register);
             Snackbar.make(button,
                     "Registered!",
                     Snackbar.LENGTH_LONG).show();
+        }else if(id == R.id.button_register && email.isEmpty() || password.isEmpty() ||
+                firstName.isEmpty() || lastName.isEmpty())
+        {
+            AlertDialog.Builder d = new AlertDialog.Builder(this);
+            d.setTitle(R.string.login_error_title);
+            d.setMessage("Please be sure to fill out all information in order to Register.");
+            d.setPositiveButton(android.R.string.ok, null);
+            d.show();
         }
+
     }
 
     /**
