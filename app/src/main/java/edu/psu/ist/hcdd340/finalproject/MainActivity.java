32d991b8452d682df1bb6d9735a5369627b505c8
Emira Y <emirahanna@gmail.com>
2024-12-15 14:38:37 -0500

    Updated HomePage
    HomePage now includes
    -Menu
    -Navigation to view Previously Made Vision Boards
    - Navigation to Character creator
    - Inspiring quotes for users to feel inspired by (or not)

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
@@ -1,37 +1,28 @@
 package edu.psu.ist.hcdd340.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public static final String SHARED_PREF_NAME = "CURRENT_STATE";
-    private SharedPreferences sharedPreferences;
-    private TextView greetingTextView;
     private TextView quoteTextView;
-    private TextView authorTextView;
-    private CardView quoteCard;
-    private CardView visionBoardCard;
-    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sharedPreferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        SharedPreferences sharedPreferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
         //not logged in
         if (!sharedPreferences.getBoolean(UserManager.CURRENT_STATE_KEY, false)) {
             Intent intent = new Intent(this, LogInActivity.class);
@@ -41,50 +32,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Bind views
-        greetingTextView = findViewById(R.id.greetingTextView);
+        TextView greetingTextView = findViewById(R.id.greetingTextView);
         quoteTextView = findViewById(R.id.quoteTextView);
-        authorTextView = findViewById(R.id.authorTextView);
-        quoteCard = findViewById(R.id.quoteCard);
-        visionBoardCard = findViewById(R.id.visionBoardCard);
+        CardView quoteCard = findViewById(R.id.quoteCard);
+        CardView visionBoardCard = findViewById(R.id.visionBoardCard);
+        CardView dreamsLifeCard = findViewById(R.id.createDreamLifeCard);
 
         // Set the greeting (You can fetch the user's name dynamically)
         String username = sharedPreferences.getString(UserManager.CURRENT_USER_KEY, "bestie");
-        greetingTextView.setText("Hello, " + username + "!");
+        String greetingText = "Welcome back,\n" + username + "!";
+        greetingTextView.setText(greetingText);
 
         // Set a random quote
-        String[] quotes = {
-                "“Dream big and dare to fail.”",
-                "“The future belongs to those who believe in the beauty of their dreams.”",
-                "“Success is not the key to happiness. Happiness is the key to success.”",
-                "“Believe you can and you're halfway there.”",
-                "“Don't watch the clock; do what it does. Keep going.”"
-        };
+        quoteTextView.setText(getRandomQuote());
+
+        // Set button functionality (using lambdas!!!) Love this for us
+        visionBoardCard.setOnClickListener(e -> createIntentAndStartActivity(this, VisionBoardActivity.class));
+        quoteCard.setOnClickListener(e-> quoteTextView.setText(getRandomQuote()));
+        dreamsLifeCard.setOnClickListener(e ->
+                createIntentAndStartActivity(this, DreamYouActivity.class));
+    }
+
+    private String getRandomQuote(){
+        String[] quotes = {"Do it for your future self.", "You’re not behind in life. There’s no timeline.", "Small progress is still progress.", "You have survived 100% of your bad days.", "Your vibe attracts your tribe.", "Dream big, start small, but most importantly, start.", "Be the energy you want to attract.", "Sometimes, what didn't work out for you really worked out for you.", "Focus on the step in front of you, not the whole staircase.", "The universe is not in a hurry. You are.", "Take a deep breath and remind yourself of who you are.", "You are allowed to be both a masterpiece and a work in progress.", "Growth is growth, no matter how small.", "Don’t just exist; live.", "Everything you want is on the other side of fear.", "Trust the timing of your life.", "Do what you can, with what you have, where you are.", "Protect your peace.", "Your potential is endless.", "Be the reason someone believes in the goodness of people.", "What is coming is better than what is gone.", "Even the darkest night will end, and the sun will rise.", "You are braver than you believe and stronger than you seem.", "Keep shining; the world needs your light.", "One day or day one. You decide.", "Your journey is not the same as anyone else’s.", "Bloom where you are planted.", "It’s not too late to start over.", "You’re allowed to take up space."};
         Random random = new Random();
-        String randomQuote = quotes[random.nextInt(quotes.length)];
-        quoteTextView.setText(randomQuote);
-
-
-        // Set click listener for Vision Board card
-        visionBoardCard.setOnClickListener(v -> {
-            Intent intent = new Intent(this, VisionBoardActivity.class);
-            startActivity(intent);
-        });
+        return quotes[random.nextInt(quotes.length)];
     }
 
-    private String[] getRandomQuoteFromCSV() {
-        List<String[]> quotes = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader("quotes.csv"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] split = line.split(",", 2); // Split into quote and author
-                    quotes.add(split);
-            }
-        } catch (IOException e) {
-           // e.printStackTrace();
-        }
+    //gives menu functionality to flip between screens
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Class changedClass = this.getClass();
+        int menuId = item.getItemId();
 
-            Random random = new Random();
-            return quotes.get(random.nextInt(quotes.size()));
+        createIntentAndStartActivity(this, AppHelper.processItemsSelectedMainMenu(menuId, changedClass, this));
+        return super.onOptionsItemSelected(item);
     }
 
+    private boolean createIntentAndStartActivity(Context context, Class cls) {
+        Intent intent = new Intent(context, cls);
+        startActivity(intent);
+        return true;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 }
